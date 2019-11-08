a69af5713a52a1d1e877498b37e65f22b56818dd
Spirillen <pdns@protonmail.com>
2019-11-08 22:18:07 +0100

    New output combining domain+wildcard in one file

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,14 @@ set -e
 
 find $CI_PROJECT_DIR/source/ -type f -name '*.list' -exec bash -c "sort -i -u \
 	-f '{}' -o '{}' " \;
+
+# Combine domain and wildcard domains for external usages
+
+cd $CI_PROJECT_DIR/
+
+find $CI_PROJECT_DIR/source/ -type f -name 'combined.txt' -delete
+
+for d in `find source/ -mindepth 1 -maxdepth 1 -type d`
+do
+    cat ${d[@]}/*.list > ${d[@]}/combined.txt
+done
---
+++ scripts/update_active_lists.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script is intended to list all active lists from the data/
+# directory for easier imports from external sources....
+# Happy havesting :)
+
+truncate -s 0 "$CI_PROJECT_DIR/sources.list"
+
+for lists in `find source/ -type f -name domain.list`
+do
+	printf "$CI_PROJECT_URL/raw/master/$lists\n" | sort -u -f >> "$CI_PROJECT_DIR/sources.list"
+done
