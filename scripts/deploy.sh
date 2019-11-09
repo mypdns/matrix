cc68820c67d30ea60acfdaf86128244cbf12f6e1
Spirillen <pdns@protonmail.com>
2019-11-09 14:05:52 +0100

    Enabled Curl api in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,12 @@
 set -e -u
 
 # Change GIT upstream from git to https
-git remote set-url origin https://mypdns:$MypDNS_CI@gitlab.com/$CI_PROJECT_PATH.git
+#git remote set-url origin https://mypdns:$MypDNS_CI@gitlab.com/$CI_PROJECT_PATH.git
 
 git add .
 git status
 git commit -am '[skip ci] commit from CI runner'
-git push -u origin master
+git push -u origin ${CI_COMMIT_REF_NAME}
 
 # Tell REPO_NAME to run the CI
-#sleep 5s curl -X POST -F token="$hosts_token" -F ref=master https://gitlab.com/api/v4/projects/14278031/trigger/pipeline
+sleep 5s curl -X POST -F token="$hosts_token" -F ref=master https://gitlab.com/api/v4/projects/14278031/trigger/pipeline
