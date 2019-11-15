b48794d5562d1f6697bd7137bfd3e133f4d2460f
Spirillen Marsupilami <3728965-spirillen@users.noreply.gitlab.com>
2019-11-15 06:34:14 +0000

    Found ibelgique.com as sharked by parkingcrew.net.
    dig ww1.ibelgique.com
    
    ;; ANSWER SECTION:
    ww1.ibelgique.com.      600     IN      CNAME   parkingcrew.net.
    parkingcrew.net.        59      IN      A       185.53.179.29

---
+++ move_sub.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#echo "Enter domain to handle as 'domain.tld': "
+
+read -p "Enter domain to handle as 'domain.tld': " domain
+
+printf "\n$domain\n"
+
+printf "\ngrep $domain\n"
+grep -i ".${domain}$" "source/porno-sites/wildcard.list" >> "source/porno-sites/domain.list"
+
+printf "\nsed $domain\n"
+sed -i "/.${domain}/d" "source/porno-sites/wildcard.list"
+
+printf "\nGit $domain\n"
+git commit -am "Moved ${domain}"
+
+grep -iE '[a-z0-9]+\.[a-z]+\.[a-z]+' source/porno-sites/wildcard.list | grep -viE '(co.(uk|jp|za))|(com.(au|br))$'
