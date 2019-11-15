c869bf25fed617b25b900a0afdeb742e68d2b085
Spirillen <pdns@protonmail.com>
2019-11-15 06:41:38 +0100

    Closes #543 youporn.biz.pl

--- move_sub.sh
+++ move_sub.sh
@@ -15,4 +15,4 @@ sed -i "/.${domain}/d" "source/porno-sites/wildcard.list"
 printf "\nGit $domain\n"
 git commit -am "Moved ${domain}"
 
-grep -iE '[a-z0-9]+\.[a-z]+\.[a-z]+' source/porno-sites/wildcard.list | grep -viE '(co.(uk|jp))|(com.(au|br))$'
+grep -iE '[a-z0-9]+\.[a-z]+\.[a-z]+' source/porno-sites/wildcard.list | grep -viE '(co.(uk|jp|za))|(com.(au|br))$'
