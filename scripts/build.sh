e34cef28601c7c017ff8ca63484438f81fd99d33
AnonymousPoster <3917527-AnonymousPoster@users.noreply.gitlab.com>
2019-11-19 23:24:37 +0000

    [skip ci]

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,7 +20,7 @@ done
 # Import latest working example of safesearch from safesearc.mypdns.cloud
 rm $CI_PROJECT_DIR/safesearch/safesearch.mypdns.cloud.rpz
 
-dig axfr @axfr.ipv4.mypdns.cloud safesearch.mypdns.cloud | grep -vE '^($|;)' >> $CI_PROJECT_DIR/safesearch/safesearch.mypdns.cloud.rpz
+dig axfr @axfr.ipv4.mypdns.cloud -p 5353 safesearch.mypdns.cloud | grep -vE '^($|;)' >> $CI_PROJECT_DIR/safesearch/safesearch.mypdns.cloud.rpz
 
 
 exit ${?}
