51e921967472b09e346910945fb338fb7b2e06d8
Spirillen <pdns@protonmail.com>
2020-01-26 11:32:35 +0100

    Adding new porno domain jav321.net [skip ci]\n\nCloses https://gitlab.com/my-privacy-dns/matrix/matrix/issues/1789\n\nThanks to My Privacy DNS Firewall https://www.mypdns.org/

--- adult.sh
+++ adult.sh
@@ -15,7 +15,11 @@ printf "\nAdding domain: $domain\n"
 printf "$domain\n" >> "source/porno-sites/wildcard.list"
 
 printf "\nGit commit $domain\nwith issue ID: $issue\n"
-git commit -am "Implement #$issue by adding ${domain} [skip ci]"
+git commit -am "Adding new porno domain ${domain} [skip ci]\n\nCloses https://gitlab.com/my-privacy-dns/matrix/matrix/issues/${issue}\n\nThanks to My Privacy DNS Firewall https://www.mypdns.org/"
+
+pdnsutil add-record "adult.mypdns.cloud" "${domain}" CNAME 86400 .
+pdnsutil increase-serial 'adult.mypdns.cloud'
+
 #git push
 
 #printf "\nsed $domain\n"
