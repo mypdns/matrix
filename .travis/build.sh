1729b99f1b6baaf531af308efdaa337aeb8b92aa
Spirillen <pdns@protonmail.com>
2020-01-29 01:36:53 +0100

    Something

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,6 +2,18 @@
 
 # Exit on any errors
 set -e
+set -x
+
+PrepareTravis () {
+    git remote rm origin
+    git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+    git config --global user.email "${GIT_EMAIL}"
+    git config --global user.name "${GIT_NAME}"
+    git config --global push.default simple
+    git checkout "${GIT_BRANCH}"
+    ulimit -u
+}
+PrepareTravis
 
 # Sort our lists for doublets and order by alphabet
 find $TRAVIS_BUILD_DIR/source/ -type f -name '*.list' -exec bash -c "sort -i -u \
--- .travis/update_active_lists.sh
+++ .travis/update_active_lists.sh
@@ -3,6 +3,8 @@
 # This script is intended to list all active lists from the data/
 # directory for easier imports from external sources....
 # Happy havesting :)
+set -e
+set -x
 
 truncate -s 0 "$TRAVIS_BUILD_DIR/source.list"
 
@@ -10,7 +12,7 @@ cd "$TRAVIS_BUILD_DIR"
 
 for lists in `find source/ -type f -name "*.list"`
 do
-	printf "https://raw.githubusercontent.com/mypdns/matrix/master/$lists\n" | sort -u >> "$TRAVIS_BUILD_DIR/source.list"
+    printf "https://raw.githubusercontent.com/mypdns/matrix/master/$lists\n" | sort -u >> "$TRAVIS_BUILD_DIR/source.list"
 done
 
-ls -lh $TRAVIS_BUILD_DIR/
+#ls -lh $TRAVIS_BUILD_DIR/
