2515a18f5978a2508a6c231889779cccf057e97c
Spirillen <pdns@protonmail.com>
2020-02-11 04:09:42 +0100

    Adding new porno domain `javdock.net` Closes https://github.com/mypdns/matrix/issues/218
    This submission enhanced the true power of My DNS Privacy Firewall
    by https://www.mypdns.org/.
    
    If you would like to learn more about how to use the RPZ powered DNS Firewall
    with our zone files, you can read more about it here
    https://www.mypdns.org/wiki/Rpz
    
    You can read about about our different zones here
    https://www.mypdns.org/wiki/RpzList
    
    [ci skip]
    
    Signed-off-by: Spirillen <pdns@protonmail.com>

--- adult.sh
+++ adult.sh
@@ -29,7 +29,7 @@ printf "\n"
 
 read -p "Enter domain to handle as 'domain.tld': " domain
 read -p "Enter GH issue ID: " issue
-
+read -p "Enter Pornhost Issue ID: " pissue
 printf "\nAdding domain: $domain\n"
 printf "$domain\n" >> "source/porno-sites/wildcard.list"
 
@@ -62,6 +62,33 @@ pdnsutil increase-serial 'adult.mypdns.cloud'
 sed -i "/${domain}/d" "./tmp/67.list"
 sed -i "/${domain}/d" "./source/porno-sites/wildcard.list.old"
 
+# Let's also commit to Import-External-Sources/pornhosts while are doing something
+
+printf "\n\tStarting to commit to pornhosts\n"
+
+cd "../../../github/pornhosts/"
+
+printf "$domain\n" >> "submit_here/hosts.txt"
+
+git commit -am "Adding new porno domain \`${domain}\`
+Closes https://github.com/Import-External-Sources/pornhosts/issues/${pissue}
+
+This submission enhanced the true power of My DNS Privacy Firewall
+by https://www.mypdns.org/.
+
+If you would like to learn more about how to use the RPZ powered DNS Firewall
+with our zone files, you can read more about it here
+https://www.mypdns.org/wiki/Rpz
+
+You can read about about our different zones here
+https://www.mypdns.org/wiki/RpzList
+
+[ci skip]"
+
+# Get back to script path (matrix)
+cd "../../gitlab/matrix/matrix/"
+
 #grep -iE '[a-z0-9]+\.[a-z]+\.[a-z]+' source/porno-sites/wildcard.list | grep -viE '(co.(uk|jp|za))|(com.(au|br))$'
 
+# Let's start over.
 ./adult.sh
