4ad951b3e0c2a81fea4d7f2a6a3bb91fff0f403c
AnonymousPoster <pdns@pm.me>
2020-04-28 23:08:13 +0200

    `Bing SafeSearch regex line` Closes T624     https://www.mypdns.org/T624
    Bugfix: As @jaydouble mention in this issue there was
    a cling to much in the bing regexrule. This should now
    be solved.
    
    Thanks for bringing this to our attention.
    
    Also Closes <https://github.com/mypdns/matrix/issues/406>

--- safesearch/dnsdist_regex.lua
+++ safesearch/dnsdist_regex.lua
@@ -5,7 +5,9 @@ search for known domains. Listed from most privacy enhanged to least privacy
 You can activate this file in your dnsdist.conf by:
 dofile(conf/dnsdist_regex.lua)
 
-And don't forget order matters
+And don't forget order matters. = Line 2 overrules line 1
+
+Have fun.
 --]]
 
 -- duckduckgo.com static
@@ -18,7 +20,7 @@ addAction('duckduckgo.com.', SpoofCNAMEAction('safe.duckduckgo.com.'))
 -- See https://gitlab.com/my-privacy-dns/matrix/matrix/issues/582
 
 -- Bing
-addAction('RegexRule("(^|\\.)bing[.][a-z]{2,5}([.][a-z]{2})?$"), SpoofCNAMEAction('strict.bing.com.'))
+addAction(RegexRule("(^|\\.)bing[.][a-z]{2,5}([.][a-z]{2})?$"), SpoofCNAMEAction('strict.bing.com.'))
 
 -- Google
 addAction(RegexRule("(^|\\.)google[.][a-z]{2,5}([.][a-z]{2})?$"), SpoofCNAMEAction('forcesafesearch.google.com.'))
@@ -31,6 +33,7 @@ addAction('youtubei.googleapis.com.', SpoofCNAMEAction('restrict.youtube.com.'))
 addAction('youtube.googleapis.com.', SpoofCNAMEAction('restrict.youtube.com.'))
 addAction('www.youtube-nocookie.com.', SpoofCNAMEAction('restrict.youtube.com.'))
 
+-- Youtube
 -- Strict moderate
 --[[
 addAction('www.youtube.com.', SpoofCNAMEAction('restrictmoderate.youtube.com.'))
