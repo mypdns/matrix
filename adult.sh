0526ed96b104b1980675f0a4fcaa9a41e88fb903
Spirillen <pdns@protonmail.com>
2020-05-18 14:04:12 +0200

    Api test
    Testing the api possibilities

--- adult.sh
+++ adult.sh
@@ -15,6 +15,18 @@
 
 set -e #-x
 
+## Set script root dir
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd $(dirname "$SCRIPT_PATH"); SCRIPT_PATH=$(readlink "${SCRIPT_PATH}"); done
+fi
+cd $(dirname "${SCRIPT_PATH}".) > /dev/null
+SCRIPT_PATH=$(pwd);
+popd  > /dev/null
+
+ROOT_DIR="$(dirname "$SCRIPT_PATH")"
+
 # Do we have pdnsutil installed on this machine?
 hash pdnsutil 2>/dev/null || { echo >&2 "pdnsutil Is required, but it's not installed.  Aborting..."; exit 1; }
 
@@ -27,14 +39,16 @@ grep -ivE '[a-z0-9]+\.[a-z]+\.[a-z]+' "./tmp/rpz.missing" | head -n 1
 
 printf "\n"
 
-read -e -r -p "Enter domain to handle as 'domain.tld': " -i "$(grep -ivE '[a-z0-9]+\.[a-z]+\.[a-z]+' './tmp/rpz.missing' | head -n 1)" domain
+read -e -r -p "Enter domain to handle as 'domain.tld': " \
+  -i "$(grep -ivE '[a-z0-9]+\.[a-z]+\.[a-z]+' './tmp/rpz.missing' | head -n 1)" \
+  domain
 
 # Request for the needs of www. for primary domain
 while true
 do
-read -r -p "Append WWW to ${domain} [Y/n] " input
+read -r -p "Append WWW to ${domain} [Y/n] " www
 
-case $input in
+case $www in
 	[yY][eE][sS]|[yY])
  _www="true"
  break
@@ -50,13 +64,35 @@ case $input in
 done
 
 read -rp "Enter Pornhost Issue ID: " issue
-#read -rp "Enter MyPdns.org Phabricator ID: " tissue
+read -rp "Enter MyPdns.org Phabricator ID: " mypdns_id
 
-printf "\nAdding domain: $domain\n"
-printf "$domain\n" >> "source/porno-sites/wildcard.list"
+# Setup some general functions and variables
+_branch_name="submit/${domain//\./_}"
 
-printf "\nGit commit $domain\nwith Pornhost issue ID: $issue\n"
-#printf "\nGit commit $domain\n MypDNS Bug: T$tissue\n"
+git_commit () {
+git checkout master
+git push origin "$_branch_name"
+}
+
+git_commit_mypdns () {
+git checkout master
+git merge --no-ff "$_branch_name"
+git push origin master
+git push origin "$_branch_name"
+git branch -D "$_branch_name"
+}
+
+###
+
+printf "Changing branch to %s\n" "$_branch_name"
+git checkout -b "submit/$_branch_name" master
+
+printf "\nAdding domain: %s\n" "$domain"
+printf "%s\n" >> "source/porno-sites/wildcard.list" "$domain"
+
+printf "\nGit commit %s\nwith Pornhost issue ID: %s\n" "$domain" "$issue"
+
+printf "\nGit commit $domain\n MypDNS Bug: T%s\n" "$mypdns_id"
 
 # https://askubuntu.com/questions/29215/how-can-i-read-user-input-as-an-array-in-bash/29582#29582
 additional=()
@@ -72,6 +108,8 @@ sed -i "/${domain}/d" "./tmp/rpz.missing"
 sed -i "/${domain}/d" "./tmp/rpz.missing.bak"
 sed -i "/${domain}/d" "./source/porno-sites/wildcard.list.old"
 
+if [ -z "${mypdns_id}" ]
+then
 git commit -am "Adding new porno domain \`${domain}\`
 
 Related issue: https://github.com/spirillen/pornhosts/issues/${issue}
@@ -85,61 +123,102 @@ https://www.mypdns.org/w/rpz
 
 You can read about about our different zones here
 https://www.mypdns.org/w/rpzList"
+else
+git commit -am "Adding new porno domain \`${domain}\`
+
+Closes T${mypdns_id}	https://www.mypdns.org/T${mypdns_id}
+
+Related issue: https://github.com/spirillen/pornhosts/issues/${issue}
+
+You can read more about this particular Porn blocking project at
+https://www.mypdns.org/project/view/10/
+
+If you would like to learn more about how to use the RPZ powered DNS
+Firewall with our zone files, you can read more about it here
+https://www.mypdns.org/w/rpz
+
+You can read about about our different zones here
+https://www.mypdns.org/w/rpzList"
+fi
+
+git_commit_mypdns
 
 # Following code will only succeed if you have admin access to our DNS
 # Servers at https://www.mypdns.org/
 # Everybody else needs to out comment the following lines
-printf "\nAdding ${domain} to our RPZ\n"
+printf "\nAdding %s to our RPZ\n" "$domain"
 pdnsutil add-record "adult.mypdns.cloud" "${domain}" CNAME 86400 .
 pdnsutil add-record "adult.mypdns.cloud" "*.${domain}" CNAME 86400 .
 
 printf "\nIncreasing serial of our RPZ\n"
 pdnsutil increase-serial 'adult.mypdns.cloud'
 
-#git push
-
-# Let's also commit to Import-External-Sources/pornhosts while are doing something
+# Let's also commit to Import-External-Sources/pornhosts while are at it
 
 printf "\n\tStarting to commit to pornhosts\n\n"
 
 cd "../../../github/pornhosts/"
 
+# Change branch
+git checkout -b "submit/$_branch_name" master
+
 # Adding primary domain
-printf "$domain\n" >> "submit_here/hosts.txt"
+printf "%s\n" >> "submit_here/hosts.txt" "$domain"
 
 # Are we going to submit the domain with or without _www.domain?
 if [ ${_www} == "true" ]
 then
-printf "www.$domain\n" >> "submit_here/hosts.txt"
+printf "www.%s\n" >> "submit_here/hosts.txt" "$domain"
 fi
 
 # Append hosts file specific requirements
-printf '%s\n' "Appending additional hosts requirements:"
-printf '%s\n' "${additional[@]}"
+if [ -n "${additional[1]}" ]
+then
+    printf '%s\n' "Appending additional hosts requirements:"
+    printf '%s\n' "${additional[@]}"
+fi
 
-# The next part should be a seperate branch with a MR
-printf "Added ${domain} Closes https://github.com/Import-External-Sources/pornhosts/issues/${issue}\n\n" >> commit.msg
 
-#git commit -am "Adding new porno domain \`${domain}\`
+if [ -z "${mypdns_id}" ]
+then
+git commit -am "Adding new porno domain \`${domain}\`
 
-#Closes https://github.com/spirillen/pornhosts/issues/${issue}
+Closes https://github.com/Import-External-Sources/pornhosts/issues/${issue}
 
-#Source: https://github.com/Sinfonietta/hostfiles/pull/100
+You can read more about this particular Porn blocking project at
+https://www.mypdns.org/project/view/10/
 
-#This submission enhanced the true power of My DNS Privacy Firewall
-#by https://www.mypdns.org/.
+If you would like to learn more about how to use the RPZ powered DNS
+Firewall with our zone files, you can read more about it here
+https://www.mypdns.org/w/rpz
 
-#If you would like to learn more about how to use the RPZ powered DNS Firewall
-#with our zone files, you can read more about it here
-#https://www.mypdns.org/w/rpz
+You can read about about our different zones here
+https://www.mypdns.org/w/rpzList"
 
-#You can read about about our different zones here
-#https://www.mypdns.org/w/rpzList
+else
 
-#[ci skip]"
+git commit -am "Adding new porno domain \`${domain}\`
+
+Closes https://github.com/Import-External-Sources/pornhosts/issues/${issue}
+
+Related issue: https://www.mypdns.org/T${mypdns_id}
+
+You can read more about this particular Porn blocking project at
+https://www.mypdns.org/project/view/10/
+
+If you would like to learn more about how to use the RPZ powered DNS
+Firewall with our zone files, you can read more about it here
+https://www.mypdns.org/w/rpz
+
+You can read about about our different zones here
+https://www.mypdns.org/w/rpzList"
+fi
+
+git_commit
 
 # Get back to script path (matrix)
-cd "../../gitlab/matrix/matrix/"
+cd "${ROOT_DIR}"
+
 
 #grep -iE '[a-z0-9]+\.[a-z]+\.[a-z]+' source/porno-sites/wildcard.list | grep -viE '(co.(uk|jp|za))|(com.(au|br))$'
 
