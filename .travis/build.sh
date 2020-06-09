9dce71b5edd26d6f3c7b7d47065cc33a0fe9c58a
Spirillen <pdns@protonmail.com>
2020-06-09 15:27:24 +0200

    axfr port :smiley:
    Simply forgot I've changed axfr port to :5306

--- .travis/build.sh
+++ .travis/build.sh
@@ -39,7 +39,7 @@ done
 # Import latest working example of safesearch from safesearc.mypdns.cloud
 rm $TRAVIS_BUILD_DIR/safesearch/safesearch.mypdns.cloud.rpz
 
-dig axfr @axfr.ipv4.mypdns.cloud -p 5353 safesearch.mypdns.cloud \
+dig axfr @axfr.ipv4.mypdns.cloud -p 5306 safesearch.mypdns.cloud \
   | grep -vE '^($|;)' >> $TRAVIS_BUILD_DIR/safesearch/safesearch.mypdns.cloud.rpz
 
 # https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
