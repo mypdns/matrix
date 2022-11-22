592d533965bb6ac8f769d3afe598f9465f56b6aa
Spirillen <pdns@protonmail.com>
2022-11-23 00:37:30 +0100

    ShadowWhisperer Adult list cron update script

---
+++ scripts/shadowwhisperer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Run this script as cron task
+
+REPO_DIR="/home/$USER/Downloads/git_projects/my_privacy_dns/matrix"
+
+GIT_DIR="$(git rev-parse --show-toplevel)"
+
+if [ -d "$REPO_DIR" ]; then
+    cd "$REPO_DIR"
+    git pull --rebase origin master
+    wget -O source/porn_filters/imported/adult.ShadowWhisperer https://raw.githubusercontent.com/ShadowWhisperer/BlockLists/master/RAW/Adult
+    git commit -am "Updated ShadowWhisperer Adult list"
+    git push origin master
+fi
