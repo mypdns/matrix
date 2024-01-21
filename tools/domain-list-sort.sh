2194a58bddb811047f8c5e6240e024888b95fdba
Spirillen <pdns@protonmail.com>
2024-01-21 05:43:45 +0100

    testing `.git/hooks/pre-commit`

---
+++ tools/domain-list-sort.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+## This script should be placed in pre-commit-hooks
+
+GIT_DIR="$(git rev-parse --show-toplevel)"
+
+if [ -d "$GIT_DIR" ]; then
+    # shellcheck disable=SC2164
+    cd "${GIT_DIR}" || exit 1
+
+    for i in $(git ls-files -m | grep -i "domains.list"); do
+        python3.11 "$GIT_DIR/tools/domain-sort.py" <"${i}" >"${i}.tmp" &&
+            sed "/^$/d" "${i}.tmp" >"${i}"
+    done
+
+fi
