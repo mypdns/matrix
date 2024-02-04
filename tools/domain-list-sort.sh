b205944f0833cf45a983f9daf1b32666c226d8ac
Spirillen <44526987+spirillen@users.noreply.github.com>
2024-02-04 19:52:03 +0100

    Why?? unexpected EOF while looking for matching `"'
    ref: https://github.com/mypdns/matrix/actions/runs/7776039019/job/21202696702#step:5:16

--- tools/domain-list-sort.sh
+++ tools/domain-list-sort.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
 
 ## This script should be placed in pre-commit-hooks
-
+set -xe
 GIT_DIR="$(git rev-parse --show-toplevel)"
 
 if [ -d "$GIT_DIR" ]; then
-    # shellcheck disable=SC2164
     cd "${GIT_DIR}" || exit 1
 
     for i in $(git ls-files -m | grep -i "domains.list"); do
