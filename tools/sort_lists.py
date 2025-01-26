3f24af2a6dbfb5ae983989b55ea30a07991fbcb4
Spirillen <44526987+spirillen@users.noreply.github.com>
2025-01-26 17:21:12 +0100

    Sort script not committing...

--- tools/sort_lists.py
+++ tools/sort_lists.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+#  <Program Name>: <Brief Description of the Program>
+#  Copyright (C) 2025. My Privacy DNS
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import sys
-import csv
 import argparse
 import webbrowser
 import re
@@ -10,9 +25,8 @@ import ipaddress
 from subprocess import check_output, CalledProcessError
 import requests
 import idna
-import json
 
-VERSION = "1.0.0"  # First stable release version
+VERSION = "0.0.2b26"  # First stable release version
 
 def find_files_by_name(directory, filenames):
     matches = []
